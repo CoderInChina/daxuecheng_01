1294086783a65c87104bdb7e314b368f0d84498a
wc <838742069@qq.com>
2019-06-26 18:17:46 +0800

    在test-brancks提交了代码

--- 01-git代码测试/src/com/shop/test/UserService.java
+++ 01-git代码测试/src/com/shop/test/UserService.java
@@ -10,4 +10,14 @@ public interface UserService {
    void updateUser();
    
    void ccc11();
+   
+   
+   // 此方法添加在master主分支上
+   void master11();
+   
+   
+   // 此方法添加在brank分支上
+   void branck1();
+   
+   
 }
